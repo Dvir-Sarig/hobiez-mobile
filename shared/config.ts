385f94a8b412c22f88546e9ca32862ba7164d288
dvirsarig1 <dvir.sarig@justplayapps.com>
2025-06-24 18:23:32 +0300

    little polishing

--- shared/config.ts
+++ shared/config.ts
@@ -3,6 +3,6 @@ import Constants from 'expo-constants';
 const { apiBaseUrl, googleMapsApiKey } = Constants.expoConfig?.extra || {};
 
 
-export const API_BASE_URL = 'http://10.0.0.5:8080';
+export const API_BASE_URL = apiBaseUrl;
 export const GOOGLE_MAPS_API_KEY = googleMapsApiKey;
 export default API_BASE_URL;
